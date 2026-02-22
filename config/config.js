383abaee90db8b879d4351876089bb3c0e13222c
abiola boluwatife <abiolaboluwatife@gmail.com>
2026-02-22 13:13:03 +0100

    seccond ccommit prac

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 console.log('config me plase')
 console.log('version 4')
 console.log('i love coding')
+console.log('a second ccommit')
--- git-area/git.js
+++ git-area/git.js
@@ -1,2 +1,3 @@
 console.log('a boy watching')
 console.log('version  3')
+console.log('second  commit git')
